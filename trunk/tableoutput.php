1cc9d297c294e0383cfdbf1087f0f996d5f02705
martyn.eggleton <martyn.eggleton@2c18ba5e-bf04-11dd-94e2-cbf2318f60c0>
2009-02-23 11:50:19 +0000

    Fixes to noticeprint

--- trunk/tableoutput.php
+++ trunk/tableoutput.php
@@ -5,17 +5,25 @@
       <?php 
         foreach($aEvents as $sDate => $aDateInfo)
         {
+          ksort($aDateInfo['aData']);
           $iDayCount = 0;
           //$aEvents[$sDate]['aData'][$iStartKey][$sName][] = array(
-          foreach($aDateInfo['aData'] as $iStartKey => $aEventsAtTime)
+          foreach(array_keys($aDateInfo['aData']) as $iStartKey)
           {
-            foreach($aEventsAtTime['REGULAR_EVENT'] as $aEvent)
+            $aEventsAtTime = $aDateInfo['aData'][$iStartKey];
+            $aRegular = $aEventsAtTime['REGULAR_EVENT'];
+            ksort($aRegular);
+            foreach($aRegular as $aEvent)
             {
-              $iDayCount ++; ?>
+              $iDayCount ++; 
+              ?>
               <tr valign="top">
-                <?php if(count($iDayCount == 1))
-                {?>
-                <td width="20%" rowspan="<?php count($aDateInfo['aData']);?>">
+                <?php if($iDayCount == 1)
+                {
+                  //echo "\n<br><pre>\nsDate  =" .$sDate ."</pre>";
+                  //echo "\n<br><pre>\niDayCount  =" .var_export($iDayCount , TRUE)."</pre>";
+                  ?>
+                <td width="20%" rowspan="<?php echo $aDateInfo['iTypeCount']['REGULAR_EVENT'];?>">
                   <font size="3" face="Arial"><?php echo date('D j\<\s\u\p\>S\<\/\s\u\p\>', $aDateInfo['iDayTime']);?></font>
                   <br/><font size="1" face="Arial"><?php 
                     echo str_replace(array('st','nd','rd','th'),array('<sup>st</sup>','<sup>nd</sup>','<sup>rd</sup>','<sup>th</sup>'),$aDateInfo['sDayName']); ?></font>
