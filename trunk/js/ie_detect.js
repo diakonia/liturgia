020969c9f204d36ccf24ddf4a14bdf659b11a0d7
martyn.eggleton <martyn.eggleton@2c18ba5e-bf04-11dd-94e2-cbf2318f60c0>
2009-06-24 22:26:24 +0000

    Splits the javascript into bits to make it easier to edit

---
+++ trunk/js/ie_detect.js
@@ -0,0 +1,7 @@
+Sexy = new SexyAlertBox();
+if($defined(Browser.Engine.trident) && Browser.Engine.trident)
+{
+  Sexy.error( 'This is "totally untested and highly dangerous" code <br/>and it don\'t work in I.E. try <a target="_blank" href="http://getfirefox.com">firefox<a>.');
+  return;
+}
+
---
+++ trunk/js/lookups.js
@@ -0,0 +1,163 @@
+
+var noticesLookup = function()
+{
+  oNoticesFetchRequest.send();
+};
+
+var readingLookup = function()
+{
+    Sexy.form($('readinglookup').get('html'), {
+      onComplete:function(returnvalue) {
+        var sText = $('bodySetSlide').get('value');
+        var sNotes = $('notesSetSlide').get('value');
+        if(returnvalue)
+        {
+          returnvalue = new Hash(returnvalue);
+          var iReturnVerse = parseInt(returnvalue.verse, 10);
+          var sName = $('nameSetSlide').get('value')+' [[book]] [[chapter]]:[[verse]]';
+          
+          var oPages = new Hash(aBibleData[returnvalue.book][returnvalue.chapter]);
+          if (oPages)
+          {
+            var iCurrPage = 0;
+            var aVerses = oPages.getKeys();
+            var iVerseKey = -1;
+            do
+            {
+                iVerseKey ++;
+            }
+            while (iReturnVerse > aVerses[iVerseKey]);
+            
+            returnvalue.page = iCurrPage = oPages[aVerses[iVerseKey]];
+            if(!iCurrPage)
+            {
+              readingLookup();
+              return;
+            }
+            returnvalue.each(function(xFieldValue, sFieldName)
+            {
+                sText = sText.replace('[['+sFieldName+']]', xFieldValue);
+                sNotes = sNotes.replace('[['+sFieldName+']]', xFieldValue);
+                sName = sName.replace('[['+sFieldName+']]', xFieldValue);
+            });
+            //$('nameSetSlide').set('value', sName);
+            $('bodySetSlide').set('value', sText);
+            $('notesSetSlide').set('value', sNotes);
+            saveSetSlide();
+          }
+          else
+          {
+            readingLookup();
+            return;
+          }
+        }
+      }
+    });
+
+};
+
+var VideoLookup = function()
+{
+  Sexy.addEvent('onShowComplete', function(e) {
+       var aSelect = this.Content.getElements('select');
+       var eSelect = aSelect[0];
+       var sSelectName = eSelect.get('name');
+       if(sSelectName !== 'existingfile')
+       {
+        return;
+       }
+       
+       eSelect.empty();
+       var myEl = new Element('option', {'value':'null', 'text':'Choose One'});
+       eSelect.adopt(myEl);
+       aFileData.video.each(function(item, index){
+         var myEl = new Element('option', {'value':item.relativefile, 'text':item.name});
+         eSelect.adopt(myEl);
+       });
+      });
+  
+  Sexy.form($('videolookup').get('html'), {
+      onComplete:function(returnvalue) {
+         if(returnvalue)
+         {
+           var sText = $('bodySetSlide').get('value');
+           var sNotes = $('notesSetSlide').get('value');
+           returnvalue = new Hash(returnvalue);
+           
+           $('bodySetSlide').set('value', '');
+           
+           var sName = aFileData.video[returnvalue.existingfile];
+           sNotes = sNotes.replace('[[file]]', returnvalue.existingfile);
+           sNotes = sNotes.replace('[[name]]', sName);
+           
+           $('notesSetSlide').set('value', sNotes);
+           saveSetSlide();
+         }
+        
+      },
+      aFileList:aFileData.video,
+      sFileType:'video'
+    });
+};
+
+
+var PresentationLookup = function()
+{
+  Sexy.addEvent('onShowComplete', function(e) {
+       var aSelect = this.Content.getElements('select');
+       var eSelect = aSelect[0];
+       var sSelectName = eSelect.get('name');
+       if(sSelectName !== 'existingfile')
+       {
+        return;
+       }
+       
+       eSelect.empty();
+       var myEl = new Element('option', {'value':'null', 'text':'Choose One'});
+       eSelect.adopt(myEl);
+       aFileData.presentation.each(function(item, index){
+         var myEl = new Element('option', {'value':item.relativefile, 'text':item.name});
+         eSelect.adopt(myEl);
+       });
+      });
+  
+  Sexy.form($('presentationlookup').get('html'), {
+      onComplete:function(returnvalue) {
+         if(returnvalue)
+         {
+           var sText = $('bodySetSlide').get('value');
+           var sNotes = $('notesSetSlide').get('value');
+           returnvalue = new Hash(returnvalue);
+           
+           $('bodySetSlide').set('value', '');
+           
+           var sName = aFileData.video[returnvalue.existingfile];
+           sNotes = sNotes.replace('[[file]]', returnvalue.existingfile);
+           sNotes = sNotes.replace('[[name]]', sName);
+           
+           $('notesSetSlide').set('value', sNotes);
+           saveSetSlide();
+         }
+        
+      },
+      aFileList:aFileData.presentation,
+      sFileType:'presentation'
+    });
+};
+
+
+var YouTubeLookup = function()
+{
+  Sexy.form($('youtubelookup1').get('html'), {
+    onComplete:function(returnvalue) {
+      var sText = $('bodySetSlide').get('value');
+      var sNotes = $('notesSetSlide').get('value');
+      if(returnvalue)
+      {
+        returnvalue = new Hash(returnvalue);
+        oYouTubeFetchRequest.send({data:returnvalue});
+      }
+    }
+  });
+};
+
