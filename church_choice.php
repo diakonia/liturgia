ab7115714898f296b31b579da629021db0f2deb1
martyn.eggleton <martyn.eggleton@2c18ba5e-bf04-11dd-94e2-cbf2318f60c0>
2012-01-05 23:08:59 +0000

    Changes for open4worship

---
+++ church_choice.php
@@ -0,0 +1,16 @@
+<form>
+<select name="church">
+<?php
+foreach ($aChurches as $sC)
+{
+?><option value="<?=$sC;?>"><?=$sC;?></option>
+<?php } ?>
+</select>
+
+<input type="submit" value="Go" />
+</form>
+<?php
+
+exit;
+
+
---
+++ printsongs.php
@@ -0,0 +1,34 @@
+<?php
+
+  require_once('core.php');
+  $oFilePath = new filepath($_REQUEST);
+  $sFullFilePath = $oFilePath->getFullFile();
+  $sContent = @file_get_contents($sFullFilePath);
+  if($sContent)
+  {
+    $xslDoc = new DOMDocument();
+    $xslDoc->load('css/'.$oFilePath->getType().'.xsl');
+    
+    $xmlDoc = new DOMDocument();
+    $xmlDoc->loadXML($sContent);
+    
+    $proc = new XSLTProcessor();
+    $proc->importStylesheet($xslDoc);
+    
+    $foDoc = $proc->transformToDoc($xmlDoc);
+    $foDoc->formatOutput   = true;
+    $sFoFile = tempnam(CONST_TEMP_PATH, "MooSongFO");
+    $sPdfFile = tempnam(CONST_TEMP_PATH, "MooSongPdf");
+    if($sFoFile && $sPdfFile)
+    {
+      $foDoc->save($sFoFile);
+      exec(CONST_FOP_PATH.' '.$sFoFile.' '.$sPdfFile);
+      header('Content-type: application/pdf');
+      header('Content-Disposition: attachment; filename="'.$oFilePath->getBaseName().'.pdf"');
+      readfile($sPdfFile);
+    }
+    exit;
+  }
+  
+  header('HTTP/1.1 404 Not Found', true, 404);
+?>
