69fdd2c9a29fcb13cefce75f625c98ec2bf03ff3
Stephen Parry <sgparry@mainscreen.com>
2015-05-25 16:38:34 +0100

    Fixed problem with disappearing backslashes - caused Windows filepaths to break.

--- save.php
+++ save.php
@@ -5,7 +5,7 @@ require_once('core.php');
 getChurch(false);
 
 $oFilePath = new filepath($_REQUEST);
-$sXML = stripslashes(urldecode($_REQUEST['xml']));
+$sXML = urldecode($_REQUEST['xml']);
 $sFullFilePath = $oFilePath->getFullFile();
 file_put_contents($sFullFilePath, $sXML);
 $oFilePath->changeGroup();
