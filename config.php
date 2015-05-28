999712ce10664ffc7e54cb4aa1b1dd72fe84be25
Stephen Parry <sgparry@mainscreen.com>
2015-05-28 12:27:44 +0100

    Minor tidy up of how config params are locally overidden

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 @include_once('localconfig.php');
 
-@define('CONST_MooToolsJS', 'js/MooTools-Core-1.5.1.js');
-@define('CONST_MooToolsExtensionsJS', 'js/MooTools-More-1.5.1.js');
-@define('CONST_SexyAlertBoxJS', 'js/sexyalertbox.v1.2.moo.js');
+define('CONST_MooToolsJS', 'js/MooTools-Core-1.5.1.js');
+define('CONST_MooToolsExtensionsJS', 'js/MooTools-More-1.5.1.js');
+define('CONST_SexyAlertBoxJS', 'js/sexyalertbox.v1.2.moo.js');
 
 $scripts = array();
 
@@ -26,39 +26,96 @@ $deferred_scripts = array(CONST_MooToolsJS,
 
 if (!defined('CONST_UseMinifiedJS'))
 {
-    @define('CONST_UseMinifiedJS', true);
+    define('CONST_UseMinifiedJS', true);
+}
+define('CONST_JSMinified', 'js/scripts.min.js.gz');
+define('CONST_DeferredJSMinified', 'js/deferred-scripts.min.js.gz');
+
+if (!defined('CONST_OpenSongData'))
+{
+    define('CONST_OpenSongData', '../OpenSong/');
+}
+if (!defined('CONST_Client_OpenSongData'))
+{
+    define('CONST_Client_OpenSongData', '/usr/share/opensong/OpenSong/');
+}
+if (!defined('CONST_ExternaFolderSeperator'))
+{
+    define('CONST_ExternaFolderSeperator', '/');
+}
+define('CONST_InternaFolderSeperator', '/');
+if (!defined('CONST_ExternaClient_FolderSeperator'))
+{
+    define('CONST_ExternaClient_FolderSeperator', '\\');
+}
+if (!defined('CONST_SundayCutOff'))
+{
+    define('CONST_SundayCutOff', 11);
+}
+if (!defined('CONST_FOP_PATH'))
+{
+    define('CONST_FOP_PATH', 'fop');
+}
+if (!defined('CONST_TEMP_PATH'))
+{
+    define('CONST_TEMP_PATH', '/tmp');
+}
+if (!defined('CONST_SVN_AUTO'))
+{
+    define('CONST_SVN_AUTO', 0);
+}
+if (!defined('CONST_GOOGLE_EVENT_FEED'))
+{
+    define('CONST_GOOGLE_EVENT_FEED', '');
+}
+if (!defined('CONST_GOOGLE_EVENT_MAX_DAYS_FUTURE'))
+{
+    define('CONST_GOOGLE_EVENT_MAX_DAYS_FUTURE', 120);
+}
+if (!defined('CONST_NETWORK_TEST_HOST'))
+{
+    define('CONST_NETWORK_TEST_HOST', 'www.w3.org');
+}
+if (!defined('CONST_DEFAULT_TIMEZONE'))
+{
+    define('CONST_DEFAULT_TIMEZONE', 'Europe/London');
+}
+define('CONST_YouTube_DL', './youtube-dl');
+if (!defined('CONST_FileGroup'))
+{
+    define('CONST_FileGroup', false);
+}
+if (!defined('CONST_SiteTitle'))
+{
+    define('CONST_SiteTitle', "Service Editor");
+}
+if (!defined('CONST_OpenSongSets'))
+{
+    define('CONST_OpenSongSets', CONST_OpenSongData . 'Sets/');
+}
+if (!defined('CONST_OpenSongSongs'))
+{
+    define('CONST_OpenSongSongs', CONST_OpenSongData . 'Songs/');
+}
+if (!defined('CONST_OpenSongVideos'))
+{
+    define('CONST_OpenSongVideos', CONST_OpenSongData . 'Videos/');
+}
+if (!defined('CONST_OpenSongImages'))
+{
+    define('CONST_OpenSongImages', CONST_OpenSongData . 'Images/');
+}
+if (!defined('CONST_OpenSongPresentations'))
+{
+    define('CONST_OpenSongPresentations', CONST_OpenSongData . 'Presentations/');
 }
 
-@define('CONST_JSMinified', 'js/scripts.min.js.gz');
-@define('CONST_DeferredJSMinified', 'js/deferred-scripts.min.js.gz');
-@define('CONST_OpenSongData', '../OpenSong/');
-@define('CONST_Client_OpenSongData', '/usr/share/opensong/OpenSong/');
-@define('CONST_ExternaFolderSeperator', '/');
-@define('CONST_InternaFolderSeperator', '/');
-@define('CONST_ExternaClient_FolderSeperator', '\\');
-@define('CONST_SundayCutOff', 11);
-@define('CONST_FOP_PATH', 'fop');
-@define('CONST_TEMP_PATH', '/tmp');
-@define('CONST_SVN_AUTO', 0);
-@define('CONST_GOOGLE_EVENT_FEED', '');
-@define('CONST_GOOGLE_EVENT_MAX_DAYS_FUTURE', 120);
-@define('CONST_NETWORK_TEST_HOST', 'www.w3.org');
-@define('CONST_DEFAULT_TIMEZONE', 'Europe/London');
-@define('CONST_YouTube_DL', './youtube-dl');
-@define('CONST_FileGroup', false);
-@define('CONST_SiteTitle', "Service Editor");
+if (!defined('CONST_VLC_BIN'))
+{
+    define('CONST_VLC_BIN', '"C:\Program Files (x86)\VideoLan\VLC.EXE"');
+}
 
-@define('CONST_OpenSongSets', CONST_OpenSongData . 'Sets/');
-@define('CONST_OpenSongSongs', CONST_OpenSongData . 'Songs/');
-@define('CONST_OpenSongVideos', CONST_OpenSongData . 'Videos/');
-@define('CONST_OpenSongImages', CONST_OpenSongData . 'Images/');
-@define('CONST_OpenSongPresentations', CONST_OpenSongData . 'Presentations/');
-
-
-@define('CONST_VLC_BIN', '"C:\OSTools\VideoLan\VLC\VLC.EXE"');
-
-//@define('CONST_DVD_CLIP_BY_TIME_CMD_1', CONST_VLC_BIN.' "dvdsimple:///D:@<<dvdtitlenumber>>" ":start-time=<<start-time>>" ":stop-time=<<stop-time>>"');
-@define('CONST_DVD_CLIP_BY_TIME_CMD_1', '<?xml version="1.0" encoding="UTF-8"?>
+define('CONST_DVD_CLIP_BY_TIME_CMD_1', '<?xml version="1.0" encoding="UTF-8"?>
 <playlist xmlns="http://xspf.org/ns/0/" xmlns:vlc="http://www.videolan.org/vlc/playlist/ns/0/">
 	<title>Playlist</title>
 	<trackList>
@@ -77,12 +134,11 @@ if (!defined('CONST_UseMinifiedJS'))
 	</extension>
 </playlist>');
 
-@define('CONST_DVD_CLIP_BY_TIME_FILENAME_1', '<<dvdtitle>>_<<dvdtitlenumber>>_<<start-time>>_<<stop-time>>');
-@define('CONST_DVD_CLIP_BY_TIME_INSTRUCTIONS_1', 'DVD:"<<dvdtitle>>" Title:<<dvdtitlenumber>> Start:<<dvdstarthours>>:<<dvdstartmin>>:<<dvdstartsecs>> End:<<dvdendhours>>:<<dvdendmin>>:<<dvdendsecs>>');
+define('CONST_DVD_CLIP_BY_TIME_FILENAME_1', '<<dvdtitle>>_<<dvdtitlenumber>>_<<start-time>>_<<stop-time>>');
+define('CONST_DVD_CLIP_BY_TIME_INSTRUCTIONS_1', 'DVD:"<<dvdtitle>>" Title:<<dvdtitlenumber>> Start:<<dvdstarthours>>:<<dvdstartmin>>:<<dvdstartsecs>> End:<<dvdendhours>>:<<dvdendmin>>:<<dvdendsecs>>');
 
 
-//@define('CONST_DVD_CLIP_BY_TIME_CMD_2', CONST_VLC_BIN.' "dvdsimple:///D:@<<dvdtitlenumber>>:<<start-chapter>>" ":start-time=<<start-time>>" ":stop-time=<<stop-time>>"');
-@define('CONST_DVD_CLIP_BY_TIME_CMD_2', '<?xml version="1.0" encoding="UTF-8"?>
+define('CONST_DVD_CLIP_BY_TIME_CMD_2', '<?xml version="1.0" encoding="UTF-8"?>
 <playlist xmlns="http://xspf.org/ns/0/" xmlns:vlc="http://www.videolan.org/vlc/playlist/ns/0/">
 	<title>Playlist</title>
 	<trackList>
@@ -102,12 +158,10 @@ if (!defined('CONST_UseMinifiedJS'))
 </playlist>');
 
 
-@define('CONST_DVD_CLIP_BY_TIME_FILENAME_2', CONST_DVD_CLIP_BY_TIME_FILENAME_1 . '_TRY_2ND');
+define('CONST_DVD_CLIP_BY_TIME_FILENAME_2', CONST_DVD_CLIP_BY_TIME_FILENAME_1 . '_TRY_2ND');
 
 
-//@define('CONST_DVD_CLIP_BY_CHAPTER_CMD_1', CONST_VLC_BIN.' "dvdsimple:///D:@<<dvdtitlenumber>>:<<start-chapter>>-<<dvdtitlenumber>>:<<stop-chapter>>"');  
-
-@define('CONST_DVD_CLIP_BY_CHAPTER_CMD_1', '<?xml version="1.0" encoding="UTF-8"?>
+define('CONST_DVD_CLIP_BY_CHAPTER_CMD_1', '<?xml version="1.0" encoding="UTF-8"?>
 <playlist xmlns="http://xspf.org/ns/0/" xmlns:vlc="http://www.videolan.org/vlc/playlist/ns/0/">
 	<title>Playlist</title>
 	<trackList>
@@ -126,8 +180,8 @@ if (!defined('CONST_UseMinifiedJS'))
 
 
 
-@define('CONST_DVD_CLIP_BY_CHAPTER_FILENAME_1', '<<dvdtitle>>_<<dvdtitlenumber>>_<<start-chapter>>-<<dvdtitlenumber>>_<<stop-chapter>>');
-@define('CONST_DVD_CLIP_BY_CHAPTER_INSTRUCTIONS_1', 'DVD:"<<dvdtitle>>" Start Title:<<dvdtitlenumber>> Start Chapter:<<start-chapter>> End Title:<<dvdtitlenumber>> Stop Chapter:<<stop-chapter>>');
+define('CONST_DVD_CLIP_BY_CHAPTER_FILENAME_1', '<<dvdtitle>>_<<dvdtitlenumber>>_<<start-chapter>>-<<dvdtitlenumber>>_<<stop-chapter>>');
+define('CONST_DVD_CLIP_BY_CHAPTER_INSTRUCTIONS_1', 'DVD:"<<dvdtitle>>" Start Title:<<dvdtitlenumber>> Start Chapter:<<start-chapter>> End Title:<<dvdtitlenumber>> Stop Chapter:<<stop-chapter>>');
 
-@define('CONST_DVD_CLIP_SCRIPT_EXTENSION', '.xspf');
+define('CONST_DVD_CLIP_SCRIPT_EXTENSION', '.xspf');
 
